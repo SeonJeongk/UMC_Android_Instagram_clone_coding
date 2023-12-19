51a89dbb6bd5504a842d7983adf83817d9f7d8dd
RakunKo <huncher00@naver.com>
2023-12-19 14:55:43 +0900

    ✨Feat: 회원가입, 로그인 ui 구현

---
+++ instagram/app/src/main/java/com/example/instagram/ui/main/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.instagram.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram.R
+import com.example.instagram.databinding.ActivityMainBinding
+import com.example.instagram.ui.home.HomeFragment
+import com.example.instagram.ui.my.MyFragment
+import com.example.instagram.ui.reels.ReelsFragment
+import com.example.instagram.ui.search.SearchFragment
+import com.example.instagram.ui.shop.ShopFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        initBottomNavigation()
+
+        setContentView(binding.root)
+    }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBottomNavi.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.reelsFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, ReelsFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.shopFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, ShopFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, MyFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+}
