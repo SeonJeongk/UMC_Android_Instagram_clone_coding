4a70665b0f5a04def1b31d0f2d2f2fa4b5b5eb6a
RakunKo <huncher00@naver.com>
2023-12-24 21:35:17 +0900

    ✨Feat: firebase 사용하여 myFragment 수정
    firebase 사용하여 myFragment 수정

--- instagram/app/src/main/java/com/example/instagram/ui/my/MyFragment.kt
+++ instagram/app/src/main/java/com/example/instagram/ui/my/MyFragment.kt
@@ -10,7 +10,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.instagram.R
+import com.example.instagram.data.entity.User
 import com.example.instagram.databinding.FragmentHomeBinding
 import com.example.instagram.databinding.FragmentMyBinding
 import com.example.instagram.databinding.FragmentReelsBinding
@@ -18,10 +20,13 @@ import com.example.instagram.ui.main.MainActivity
 import com.example.instagram.ui.search.SearchDatailFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MyFragment : Fragment() {
     private lateinit var binding : FragmentMyBinding
-
+    private var uid : String? = ""
     private val information = arrayListOf(
         R.drawable.ic_vector,
         R.drawable.ic_union
@@ -34,6 +39,9 @@ class MyFragment : Fragment() {
     ): View? {
         binding = FragmentMyBinding.inflate(layoutInflater)
 
+        uid = arguments?.getString("uid")
+        Log.d("uid",uid.toString())
+
         // ViewPager Adapter 연결
         val myVPAdapter = MyVPAdapter(this)
         binding.myContentVp.adapter = myVPAdapter
@@ -42,23 +50,65 @@ class MyFragment : Fragment() {
             tab, position ->
             tab.setIcon(information[position])
         }.attach()
-
-        profileEdit()
+        getUserInfo(uid.toString())
 
         clickListener()
 
         return binding.root
     }
-    private fun profileEdit() {
-        var name = arguments?.getString("name")
-        var username = arguments?.getString("username")
-        var info = arguments?.getString("info")
 
-        if(name != null) {
-            binding.myUsername2Tv.text = name
-            binding.myUsernameTv.text = username
-            binding.myIntroductionTv.text = info
+    override fun onResume() {
+        super.onResume()
+        getUserInfo(uid.toString())  //새로 가져오기
+    }
+    private fun getUserInfo(uid : String){
+        val db = FirebaseFirestore.getInstance()
+
+        val documentRef = db.collection("clients").document(uid)
+
+        documentRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                val user: User? = documentSnapshot.toUser()
+
+                if (user != null) {
+                    // 사용자 객체를 사용할 수 있습니다.
+
+                    Log.d("Firestore", user.toString())
+                    profileEdit(user)
+                } else {
+                    // 변환에 실패하거나 문서가 존재하지 않는 경우
+                    Log.d("Firestore", "Failed to convert DocumentSnapshot to User")
+                }
+            }
+            .addOnFailureListener { exception ->
+                // 오류 처리
+                Log.e("Firestore", "Error getting document: ", exception)
+            }
+    }
+    fun DocumentSnapshot.toUser(): User? {
+        return try {
+            val uid = getString("uid") ?: ""
+            val username = getString("username") ?: ""
+            val name = getString("name") ?: ""
+            val email = getString("email") ?: ""
+            val profile_img = getString("profile_img") ?: ""
+            val profile_info = getString("profile_info") ?: ""
+
+            User(email, name, profile_img, profile_info, uid, username)
+        } catch (e: Exception) {
+            null // 변환에 실패한 경우 null 반환 또는 예외 처리를 수행할 수 있습니다.
         }
+    }
+    private fun profileEdit(user:User) {
+        if (user.profile_img != null) {
+            Glide.with(this)
+                .load(user.profile_img) // 불러올 이미지 url
+                .circleCrop() // 동그랗게 자르기
+                .into(binding.myProfileImageIv) // 이미지를 넣을 뷰
+        }
+        binding.myUsername2Tv.text = user.name
+        binding.myUsernameTv.text = user.username
+        binding.myIntroductionTv.text = user.profile_info
     }
 
     private fun clickListener() {
@@ -67,8 +117,16 @@ class MyFragment : Fragment() {
         }
     }
     private fun changeFragmentToProfileEdit() {
+
+        val bundle = Bundle().apply {
+            putString("uid", uid)
+        }
+        val profileEditFragment = MyProfileEditFragment()
+        // 프래그먼트에 번들 설정
+        profileEditFragment.arguments = bundle
+
         (context as MainActivity).supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frame, MyProfileEditFragment()).addToBackStack(tag)
+            .replace(R.id.main_frame, profileEditFragment).addToBackStack(tag)
             .commitAllowingStateLoss()
     }
 }
